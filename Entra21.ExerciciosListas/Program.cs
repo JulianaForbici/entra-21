0a84e9e4d8545c2862603afd3ba8806f55d625db
JulianaForbici <julianaforbicii@gmail.com>
2022-06-06 21:02:14 -0300

    Feito Program.cs dos exércicios.

--- Entra21.ExerciciosListas/Program.cs
+++ Entra21.ExerciciosListas/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Entra21.ExerciciosFor;
+using System;
+
+Console.WriteLine(@"----------------------MENU----------------------)
+1- Exercicio01
+2- Exercicio02
+3- Exercicio03");
+
+Console.WriteLine("Digite a opção desejada: ");
+int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+if(opcaoDesejada == 1)
+{
+    Exercicio01 Exercicio1 = new Exercicio01();
+    Exercicio1.Executar();
+}
+else if(opcaoDesejada == 2)
+{
+    Exercicio02 Exercicio2 = new Exercicio02();
+    Exercicio2.Executar();
+}
+else if (opcaoDesejada == 3)
+{
+    Exercicio03 Exercicio3 = new Exercicio03();
+    Exercicio3.Executar();
+
+}
